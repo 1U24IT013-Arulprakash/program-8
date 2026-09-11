d409747b4c74923ec0a251a082b2b4c0e19f4454
1U24IT013-Arulprakash <marulprakash35@gmail.com>
2026-09-11 11:36:03 +0530

    Update firewall_services.sh

--- firewall_services.sh
+++ firewall_services.sh
@@ -3,6 +3,48 @@
 # ==========================================
 # Managing Firewall Services using firewalld
 #
+# Student Name:arul prakash
+# Roll Number:1U24IT013
+# ==========================================
+
+
+# Check current firewall services
+firewall-cmd --list-services
+
+
+
+# Allow HTTP service
+firewall-cmd --add-service=http
+
+
+# Allow HTTPS service
+firewall-cmd --add-service=https
+
+
+
+
+
+# Display enabled services
+firewall-cmd --list-services
+
+
+# Remove HTTP service
+firewall-cmd --remove-service=http
+
+
+
+
+# Allow SSH permanently
+firewall-cmd --add-service=ssh --permanent
+
+
+
+# Reload firewall configuration
+firewall-cmd --reload#!/bin/bash
+
+# ==========================================
+# Managing Firewall Services using firewalld
+#
 # Student Name:
 # Roll Number:
 # ==========================================
